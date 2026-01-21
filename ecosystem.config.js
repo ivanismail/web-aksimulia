b445f30c5ec160920d9cee2ede7040815cf384da
ivansmail <ivanismail2@gmail.com>
2026-01-21 16:04:42 +0700

    add: setting port

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "aksimulia114",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001
+      }
+    }
+  ]
+}
